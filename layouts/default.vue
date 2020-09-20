bf5f2bd9987e40eb88d9af0b16011a027368c5fb
Nywton Barros <nywton.barros@gmail.com>
2020-09-20 12:07:30 -0300

    Initial Commit - Navbar - Routers

---
+++ layouts/default.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>
+    <TheNavBar />
+    <Nuxt />
+  </div>
+</template>
+<script>
+  import TheNavBar from "@/components/TheNavBar";
+  export default {
+    components: {
+      TheNavBar
+    }
+  }
+</script>
