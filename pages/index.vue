3c501f325b173e54ed9c239f694a1d5bdec819e3
Nywton Barros <nywton.barros@gmail.com>
2020-09-20 12:33:18 -0300

    - Added Vuex Store

--- pages/index.vue
+++ pages/index.vue
@@ -9,14 +9,13 @@
         My gnarly Nuxt.js project
       </h2>
       <div class="links">
-        <a
-          href="https://nuxtjs.org/"
-          target="_blank"
-          class="button--green">Documentation</a>
-        <a
-          href="https://github.com/nuxt/nuxt.js"
-          target="_blank"
-          class="button--grey">GitHub</a>
+        <nuxt-link
+          v-for="post in posts"
+          :to="{name: 'posts-id', params: {id: post.id}}"
+          class="button--green"
+        >
+          {{post.title}}
+        </nuxt-link>
       </div>
     </div>
   </section>
@@ -27,6 +26,11 @@ import Logo from '~/components/Logo.vue'
 export default {
   components: {
     Logo
+  },
+  computed: {
+    posts() {
+      return this.$store.state.posts.all;
+    }
   }
 }
 </script>
